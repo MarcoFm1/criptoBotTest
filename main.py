cbdc1d2a04188f8eced3d0376e6b708e06ef6672
Marco Fini Minue <marco.fini22@gmail.com>
2023-04-07 01:01:11 -0300

    prototipo alpha 0.02v

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
-#Descargamos e importamos la libreria discord.py creada por la comunidad
-import os
-import discord
-from discord.ext import commands
+import requests
 import pandas as pd
 import time
-from binance.client import Client
 
-api_key = os.environ["ACA VA LA API_KEY"]
-api_secret = os.environ["ACA VA LA API_SECRET"]
-client = Client(api_key, api_secret, testnet=True)
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': 'c32172ed-101a-4372-8ea9-d923f19f84e8'
+}
 
-print("Hello World!")
+cripto = str(input("Introduce la abreviacion de la cripto: "))
 
+params = {
+  'symbol': cripto
+}
+
+max_price = 0
+id = 0
+
+price_df = pd.DataFrame(columns=['Simbolo', 'Precio'])
+
+while True:
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+
+    price = data['data'][f'{cripto}']['quote']['USD']['price']
+    timestamp = pd.Timestamp.now()
+
+    price_df.loc[timestamp] = [f'{cripto}', price]
+    
+  # Verifica si el precio actual es menor que el precio máximo registrado menos una cantidad arbitraria.
+    if price < max_price - 0.01:
+        print("=========================================")
+        print('El precio de la cripto ha bajado.')
+
+        max_price = price
+        print(price_df)
+
+
+    else:
+        print("=========================================")
+        print("EL PRECIO SUBIO!")
+        print(price_df)
+
+    max_price = max(price, max_price)
+
+    time.sleep(30)
