5e8e7ef69324d00889d3ea91ef219e0cedb9b913
Marco Fini Minue <marco.fini22@gmail.com>
2023-04-06 21:02:40 -0300

    algunas librerias

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #Descargamos e importamos la libreria discord.py creada por la comunidad
+import os
 import discord
 from discord.ext import commands
+import pandas as pd
+import time
+from binance.client import Client
+
+api_key = os.environ["ACA VA LA API_KEY"]
+api_secret = os.environ["ACA VA LA API_SECRET"]
+client = Client(api_key, api_secret, testnet=True)
 
 print("Hello World!")
 
